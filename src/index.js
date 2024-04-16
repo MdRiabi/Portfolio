56626f256876df0ff2a70635473e2ab5a92e26e1
MdRiabi <mhdriabi2013@gmail.com>
2024-04-16 02:08:06 +0200

    delete the components and fixing the main route

--- src/index.js
+++ src/index.js
@@ -24,22 +24,18 @@ import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
 import Index from "views/Index.js";
-import Landing from "views/examples/Landing.js";
-import Login from "views/examples/Login.js";
-import Profile from "views/examples/Profile.js";
-import Register from "views/examples/Register.js";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" exact element={<Index />} />
-      <Route path="/landing-page" exact element={<Landing />} />
-      <Route path="/login-page" exact element={<Login />} />
-      <Route path="/profile-page" exact element={<Profile />} />
-      <Route path="/register-page" exact element={<Register />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <switch>
+        <Route path="/" exact render={props =><Index {...props}/>} />
+        <redirect to="/" />
+      </switch>
+  
     </Routes>
   </BrowserRouter>
 );
