f716a6ecbd594239e2fb5bfabf8f8b8f63128775
MdRiabi <mhdriabi2013@gmail.com>
2024-04-17 02:09:47 +0200

    refactoring the SocialMediaButtonComponent in The Footer Section

---
+++ src/components/Buttons/SocialMediaButtons.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button, UncontrolledTooltip } from 'reactstrap'
+
+function SocialMediaButtons  ({description, icon, color, link}) {
+    return (
+        <>
+        <Button
+                  className="btn-icon-only rounded-circle"
+                  color={color}
+                  href={link}
+                  id={`button-${icon}`}
+                  target="_blank"
+                >
+                  <span className="btn-inner--icon">
+                    <i className={`fa fa-${icon}`} />
+                  </span>
+                </Button>
+                <UncontrolledTooltip delay={0} target={`button-${icon}`}>
+                  {description}
+                </UncontrolledTooltip>
+        </>
+    )
+}
+
+export default   SocialMediaButtons
--- src/components/Footers/SimpleFooter.js
+++ src/components/Footers/SimpleFooter.js
@@ -16,6 +16,7 @@
 
 */
 /*eslint-disable*/
+import SocialMediaButtons from "components/Buttons/SocialMediaButtons";
 import React from "react";
 // reactstrap components
 import {
@@ -45,115 +46,52 @@ class SimpleFooter extends React.Component {
                 </h4>
               </Col>
               <Col className="text-lg-center btn-wrapper" lg="6">
-                <Button
-                  className="btn-icon-only rounded-circle"
-                  color="twitter"
-                  href="https://twitter.com/creativetim"
-                  id="tooltip475038074"
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-twitter" />
-                  </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip475038074">
-                  Follow us
-                </UncontrolledTooltip>
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="facebook"
-                  href="https://www.facebook.com/creativetim"
-                  id="tooltip837440414"
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-facebook-square" />
-                  </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip837440414">
-                  Like us
-                </UncontrolledTooltip>
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="dribbble"
-                  href="https://dribbble.com/creativetim"
-                  id="tooltip829810202"
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-dribbble" />
-                  </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip829810202">
-                  Follow us
-                </UncontrolledTooltip>
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="github"
-                  href="https://github.com/creativetimofficial"
-                  id="tooltip495507257"
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-github" />
-                  </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip495507257">
-                  Star on Github
-                </UncontrolledTooltip>
+
+              <SocialMediaButtons
+              color= 'twitter'
+              link= 'https://twitter.com/'
+              icon='twitter'
+              description= 'Follow us'
+              />  
+              
+              <SocialMediaButtons
+              color= 'facebook'
+              link= 'https://www.facebook.com/creativetim'
+              icon='facebook-square'
+              description= 'Like us'
+              />  
+              
+              <SocialMediaButtons
+              color= 'dribbble'
+              link= 'https://twitter.com/'
+              icon='dribbble'
+              description= 'Follow us'
+              />  
+              
+              <SocialMediaButtons
+              color= 'github'
+              link= 'https://github.com/creativetimofficial'
+              icon='github'
+              description= 'Star on Github'
+              />  
+
+
+              
               </Col>
             </Row>
             <hr />
-            <Row className=" align-items-center justify-content-md-between">
-              <Col md="6">
+            
                 <div className=" copyright">
                   © {new Date().getFullYear()}{" "}
                   <a
                     href="https://www.creative-tim.com?ref=adsr-footer"
                     target="_blank"
                   >
-                    Creative Tim
+                    Creative Ryaby
                   </a>
                   .
                 </div>
-              </Col>
-              <Col md="6">
-                <Nav className=" nav-footer justify-content-end">
-                  <NavItem>
-                    <NavLink
-                      href="https://www.creative-tim.com?ref=adsr-footer"
-                      target="_blank"
-                    >
-                      Creative Tim
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://www.creative-tim.com/presentation?ref=adsr-footer"
-                      target="_blank"
-                    >
-                      About Us
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="http://blog.creative-tim.com?ref=adsr-footer"
-                      target="_blank"
-                    >
-                      Blog
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md"
-                      target="_blank"
-                    >
-                      MIT License
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-              </Col>
-            </Row>
+              
           </Container>
         </footer>
       </>
