0e16c91ed2c5ce5bfdce20d132f2129b27ffcbec
MdRiabi <mhdriabi2013@gmail.com>
2024-04-18 02:10:16 +0200

    factoring SimpleCard component the returnCard does't work

--- src/components/TreeCards/TreeCards.js
+++ src/components/TreeCards/TreeCards.js
@@ -1,3 +1,4 @@
+import SimpleCard from 'components/Cards/SimpleCard'
 import React from 'react'
 import { Badge, Button, Card, CardBody, Col, Container, Row } from 'reactstrap'
 
@@ -9,112 +10,39 @@ function TreeCards() {
             <Col lg="12">
               <Row className="row-grid">
                 <Col lg="4">
-                  <Card className="card-lift--hover shadow border-0">
-                    <CardBody className="py-5">
-                      <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
-                        <i className="ni ni-check-bold" />
-                      </div>
-                      <h6 className="text-primary text-uppercase">
-                        Download Argon
-                      </h6>
-                      <p className="description mt-3">
-                        Argon is a great free UI package based on Bootstrap
-                        4 that includes the most important components and
-                        features.
-                      </p>
-                      <div>
-                        <Badge color="primary" pill className="mr-1">
-                          design
-                        </Badge>
-                        <Badge color="primary" pill className="mr-1">
-                          system
-                        </Badge>
-                        <Badge color="primary" pill className="mr-1">
-                          creative
-                        </Badge>
-                      </div>
-                      <Button
-                        className="mt-4"
-                        color="primary"
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Learn more
-                      </Button>
-                    </CardBody>
-                  </Card>
+                  
+
+                 <SimpleCard
+                 icon= 'spaceship'
+                 color='primary'
+                 title='Frontend'
+                 description='Check my Frontend project'
+                 badges= {["React","Angular","Vue","JavaScript"]}
+
+                 />
+                  
                 </Col>
                 <Col lg="4">
-                  <Card className="card-lift--hover shadow border-0">
-                    <CardBody className="py-5">
-                      <div className="icon icon-shape icon-shape-success rounded-circle mb-4">
-                        <i className="ni ni-istanbul" />
-                      </div>
-                      <h6 className="text-success text-uppercase">
-                        Build Something
-                      </h6>
-                      <p className="description mt-3">
-                        Argon is a great free UI package based on Bootstrap
-                        4 that includes the most important components and
-                        features.
-                      </p>
-                      <div>
-                        <Badge color="success" pill className="mr-1">
-                          business
-                        </Badge>
-                        <Badge color="success" pill className="mr-1">
-                          vision
-                        </Badge>
-                        <Badge color="success" pill className="mr-1">
-                          success
-                        </Badge>
-                      </div>
-                      <Button
-                        className="mt-4"
-                        color="success"
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Learn more
-                      </Button>
-                    </CardBody>
-                  </Card>
+                 
+                 <SimpleCard
+                 icon= 'ui-04'
+                 color='success'
+                 title='Backend'
+                 description='Check my Backend project'
+                 badges= {["NodeJs","SpringBoot","Dino","Embert"]}
+
+                 />
                 </Col>
                 <Col lg="4">
-                  <Card className="card-lift--hover shadow border-0">
-                    <CardBody className="py-5">
-                      <div className="icon icon-shape icon-shape-warning rounded-circle mb-4">
-                        <i className="ni ni-planet" />
-                      </div>
-                      <h6 className="text-warning text-uppercase">
-                        Prepare Launch
-                      </h6>
-                      <p className="description mt-3">
-                        Argon is a great free UI package based on Bootstrap
-                        4 that includes the most important components and
-                        features.
-                      </p>
-                      <div>
-                        <Badge color="warning" pill className="mr-1">
-                          marketing
-                        </Badge>
-                        <Badge color="warning" pill className="mr-1">
-                          product
-                        </Badge>
-                        <Badge color="warning" pill className="mr-1">
-                          launch
-                        </Badge>
-                      </div>
-                      <Button
-                        className="mt-4"
-                        color="warning"
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Learn more
-                      </Button>
-                    </CardBody>
-                  </Card>
+                 
+                 <SimpleCard
+                 icon= 'palette'
+                 color='warning'
+                 title='Design'
+                 description='Check my Design project'
+                 badges= {["React","Angular","Vue","JavaScript"]}
+
+                 />
                 </Col>
               </Row>
             </Col>
